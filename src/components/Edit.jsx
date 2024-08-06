25b5a709acfc486dd7804dc45328898f3eb6a973
adimond24 <adimond24@s.tooeletech.edu>
2024-08-06 13:52:55 -0600

    part 1

---
+++ src/components/Edit.jsx
@@ -0,0 +1,140 @@
+import React, {useState, useEffect} from "react";
+import { useParams, useNavigate} from "react-router";
+
+export default function Edit(){
+    cont [Form, setForm] = useState({
+        firstName: "",
+        lastName:"",
+        email:"",
+        age:"",
+        currentCollege:"",
+        records: [],
+    });
+    const params = useParams();
+    const navigate = useNavigate();
+
+    //test line.
+
+    useEffect(() => {
+        async function fetchData(){
+            const id = params.id.toString();
+            const response = await fetch(
+                `mongodb+srv://john:2025Ad69563@nodeexpressprojects.dvz4bvx.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects/{params.id.toString()}`
+            );
+            if(!response.ok){
+                const message = `An error has occured: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+            const record = await response.json();
+            if(!record){
+                window.alert(`Record with id ${id} not found`);
+                navigate("/");
+                return;
+            }
+
+            setForm(record);
+        }
+
+        fetchData();
+
+        return;
+    }, [params.id, navigate]);
+    //These methods will update the state properties.
+    function updateForm(value){
+        return setForm((prev) =>{
+            return{...prev, ...value};
+        });
+    }
+    async function onSubmit(e){
+        e.preventDefault();
+        const editedPerson = {
+            firstName: form.firstName,
+            lastName: form.lastName,
+            email: form.email,
+            age: form.age, 
+            currentCollege: form.currentCollege,
+        };
+        //This will send a post request to update the data in the database.
+        await fetch(`mongodb+srv://john:2025Ad69563@nodeexpressprojects.dvz4bvx.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects/${params.id}`,{
+            method:"PUT",
+            body: JSON.stringify(editedPerson),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        navigate("/")
+    }
+
+    //This following section will display the form that takes input from the user to update the data.
+    return(
+        <div>
+            <h3>Update Record</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="firstName">First Name:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="firstName"
+                        value={form.firstName}
+                        onChange={(e) => updateForm({firstName: e.target.value})}
+                    >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="lastName">Last Name: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="lastName"
+                        value={form.lastName}
+                        onChange={(e) => updateFrom({ lastName: e.target.value})}
+                    >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email"> Email: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="email"
+                        value={form.email}
+                        onChange={(e)=> updateForm({email: e.target.value})}
+                    ></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="age">Music Genre</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="age"
+                        value={form.age}
+                        onChange={(e) => updateForm({age: e.target.value})}
+                    ></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="currentCollege">Current College</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="currentCollege"
+                        value={form.currentCollege}
+                        onChange={(e)=> updateForm({currentCollege: e.target.value})}
+                    >
+                    </input>
+                </div>
+
+                <br></br>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Update Record"
+                        className="btn btn-primary"
+                    ></input>
+                </div>
+            </form>
+        </div>
+    );
+}
